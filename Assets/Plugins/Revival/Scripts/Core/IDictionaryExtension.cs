3b8c16616d531cefe0f2357d7dbfe313d12b0d03
mattak <mattak.me@gmail.com>
2018-06-23 23:56:21 +0900

    issues/1 :seedling: Create dictionary utility

---
+++ Assets/Plugins/Revival/Scripts/Core/IDictionaryExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Revival
+{
+    public static class IDictionaryExtension
+    {
+        public static TValue GetOrDefault<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
+        {
+            return dictionary.ContainsKey(key) ? dictionary[key] : default(TValue);
+        }
+    }
+}
---
+++ Assets/Plugins/Revival/Scripts/Core/IDictionaryListExtension.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Revival
+{
+    public static class IDictionaryListExtension
+    {
+        public static void AddSafely<TKey, TValue>(
+            this IDictionary<TKey, IList<TValue>> dictionary,
+            TKey key,
+            TValue value
+        )
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary[key] = default(IList<TValue>);
+            }
+
+            if (dictionary[key] == default(IList<TValue>))
+            {
+                dictionary[key] = new List<TValue>();
+            }
+
+            dictionary[key].Add(value);
+        }
+
+        public static IList<TValue> GetSafely<TKey, TValue>(
+            this IDictionary<TKey, IList<TValue>> dictionary,
+            TKey key
+        )
+        {
+            return dictionary.ContainsKey(key) ? dictionary[key] : new List<TValue>();
+        }
+    }
+}
