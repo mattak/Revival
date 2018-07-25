846a2770486ade5ef8f303535feef5cd193d1ce7
mattak <mattak.me@gmail.com>
2018-07-26 02:09:41 +0900

    :seedling: feat/#3 Revise ButtonReplay to refer Input.Replay

--- Assets/Plugins/Revival/Scripts/Core/Input.cs
+++ Assets/Plugins/Revival/Scripts/Core/Input.cs
@@ -3,5 +3,23 @@
     public static class Input
     {
         public static IInput Instance = new MemoryInputLogger(new DefaultInput());
+
+        public static void Replay()
+        {
+            if (Instance is IInputLogger)
+            {
+                var logger = (IInputLogger) Instance;
+                Instance = new MemoryInputReplayer(logger);
+            }
+
+            Instance.Start();
+        }
+
+        public static void Record()
+        {
+            var logger = new MemoryInputLogger(new DefaultInput());
+            Instance = logger;
+            logger.Start();
+        }
     }
 }
