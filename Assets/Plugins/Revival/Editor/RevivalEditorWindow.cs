4529ef5337e722eed3b755078a2b7a1e0e52bf80
mattak <mattak.me@gmail.com>
2018-07-26 02:10:21 +0900

    :seedling: feat/#3 Create simple editor for replay, stop

---
+++ Assets/Plugins/Revival/Editor/RevivalEditorWindow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Revival
+{
+    public class RevivalEditorWindow : EditorWindow
+    {
+        [MenuItem("Window/Revival")]
+        static void Init()
+        {
+            var window = (RevivalEditorWindow) GetWindow(typeof(RevivalEditorWindow), false, "Revival");
+            window.Show();
+        }
+
+        void OnGUI()
+        {
+            if (GUILayout.Button("Replay"))
+            {
+                Input.Replay();
+            }
+
+            if (GUILayout.Button("Record"))
+            {
+                Input.Record();
+            }
+        }
+    }
+}
